0c1781069d4f2312579479900a235e83fe238257
arthurmonteiro <amonteirom96@gmail.com>
2025-01-14 04:53:26 -0300

    change theme

--- lua/ppy/init.lua
+++ lua/ppy/init.lua
@@ -4,53 +4,53 @@ local M = {}
 M.colors = {
     light = {
         bg = "#FFFFFF",
-        fg = "#000000",
-        accent = "#FF66AA",
+        fg = "#2B2B2B",
+        accent = "#FF015B",
         string = "#067D17",
         keyword = "#0033B3",
         function_name = "#00627A",
         constant = "#871094",
         number = "#1750EB",
-        operator = "#000000",
+        operator = "#2B2B2B",
         comment = "#8C8C8C",
         type = "#000080",
-        variable = "#000000",
+        variable = "#2B2B2B",
         error = "#FF0000",
-        warning = "#FFA500",
-        info = "#00FFFF",
+        warning = "#FF8C00",
+        info = "#00B5FF",
         hint = "#008000",
-        line_number = "#999999",
-        line_number_cursor = "#000000",
+        line_number = "#B3B3B3",
+        line_number_cursor = "#2B2B2B",
         cursor_line_bg = "#FFFAE3",
-        selection_bg = "#C2E8FF",
+        selection_bg = "#A6D2FF",
         pmenu_bg = "#F2F2F2",
-        pmenu_fg = "#000000",
-        pmenu_select_bg = "#C2E8FF",
-        visual_bg = "#C2E8FF",
+        pmenu_fg = "#2B2B2B",
+        pmenu_select_bg = "#A6D2FF",
+        visual_bg = "#A6D2FF",
     },
     dark = {
-        bg = "#1E1E1E",
-        fg = "#CCCCCC",
-        accent = "#FF66AA",
+        bg = "#2B2B2B",
+        fg = "#A9B7C6",
+        accent = "#FF015B",
         string = "#6A8759",
         keyword = "#CC7832",
         function_name = "#FFC66D",
         constant = "#9876AA",
         number = "#6897BB",
-        operator = "#CCCCCC",
+        operator = "#A9B7C6",
         comment = "#808080",
         type = "#A9B7C6",
-        variable = "#CCCCCC",
+        variable = "#A9B7C6",
         error = "#FF0000",
-        warning = "#FFA500",
-        info = "#00FFFF",
+        warning = "#FF8C00",
+        info = "#00B5FF",
         hint = "#008000",
         line_number = "#606366",
-        line_number_cursor = "#CCCCCC",
+        line_number_cursor = "#A9B7C6",
         cursor_line_bg = "#323232",
         selection_bg = "#214283",
         pmenu_bg = "#3C3F41",
-        pmenu_fg = "#CCCCCC",
+        pmenu_fg = "#A9B7C6",
         pmenu_select_bg = "#214283",
         visual_bg = "#214283",
     }
@@ -75,12 +75,17 @@ function M.set_colors()
     vim.api.nvim_set_hl(0, "Comment", { fg = colors.comment, italic = true })
     vim.api.nvim_set_hl(0, "String", { fg = colors.string })
     vim.api.nvim_set_hl(0, "Number", { fg = colors.number })
+    vim.api.nvim_set_hl(0, "Float", { fg = colors.number })
+    vim.api.nvim_set_hl(0, "Boolean", { fg = colors.keyword })
     vim.api.nvim_set_hl(0, "Function", { fg = colors.function_name })
     vim.api.nvim_set_hl(0, "Keyword", { fg = colors.keyword })
     vim.api.nvim_set_hl(0, "Constant", { fg = colors.constant })
     vim.api.nvim_set_hl(0, "Type", { fg = colors.type })
     vim.api.nvim_set_hl(0, "Identifier", { fg = colors.variable })
     vim.api.nvim_set_hl(0, "Operator", { fg = colors.operator })
+    vim.api.nvim_set_hl(0, "Special", { fg = colors.keyword })
+    vim.api.nvim_set_hl(0, "Statement", { fg = colors.keyword })
+    vim.api.nvim_set_hl(0, "PreProc", { fg = colors.keyword })
 
     -- Popup menu
     vim.api.nvim_set_hl(0, "Pmenu", { fg = colors.pmenu_fg, bg = colors.pmenu_bg })
@@ -91,6 +96,12 @@ function M.set_colors()
     vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = colors.warning })
     vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = colors.info })
     vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = colors.hint })
+
+    -- Diff
+    vim.api.nvim_set_hl(0, "DiffAdd", { fg = colors.string, bold = true })
+    vim.api.nvim_set_hl(0, "DiffChange", { fg = colors.constant, bold = true })
+    vim.api.nvim_set_hl(0, "DiffDelete", { fg = colors.error, bold = true })
+    vim.api.nvim_set_hl(0, "DiffText", { fg = colors.fg, bold = true })
 
     -- Adicione mais grupos de destaque conforme necessário
 end
@@ -108,5 +119,3 @@ function M.load()
 end
 
 return M
-
-
